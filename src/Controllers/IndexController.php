14344ee843f6338828e13c5204afe28b7253c6e3
Jesus Farfan <jesu.farfan@gmail.com>
2016-11-21 23:24:19 -0300

    Add Request and App in order to use depency inyection

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -2,20 +2,22 @@
 
 namespace App\Controllers;
 
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-/**
- * 
- */
 class IndexController extends Controller
 {
    
-    public function indexAction()
+    public function indexAction(Request $request, Application $app)
     {
         return new JsonResponse(['data' => 'Hello!']);
     }
 
-    public function helloAction($name) {
+    public function helloAction(Request $request, Application $app) 
+    {
+        $name = $request->attributes->get('name');
+
         return new JsonResponse(['data' => $name]);
     }
 }
