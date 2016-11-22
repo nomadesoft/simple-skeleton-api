9043d2af40762037a5ae7b7529adb69b04a9c4c4
Jesus Farfan <jesu.farfan@gmail.com>
2016-11-21 23:23:33 -0300

    Add error handling

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -14,9 +14,14 @@ $app->register(new App\Providers\RouteServiceProvider());
 // Register Providers
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
 
-// Load controllers
-$app->get('/', function(){
-    return "Hello!!";
+// App error handling
+$app->error(function(\Exception $e, \Symfony\Component\HttpFoundation\Request $request, $code) {
+    if ($app['debug']) {
+        return $app->json([
+            'status' => $code,
+            'errors' => $e->getMessage(),
+        ], $code);
+    }
 });
 
 return $app;
