5258dc304e634a2da0053edfd6f5198cefe95a0e
Jesus Farfan <jesu.farfan23@gmail.com>
2016-11-30 17:44:21 -0300

    Add support multiple config files

--- src/Providers/ConfigServiceProvider.php
+++ src/Providers/ConfigServiceProvider.php
@@ -10,6 +10,12 @@ use Symfony\Component\Yaml\Exception\ParseException;
 
 class ConfigServiceProvider implements ServiceProviderInterface
 {
+    private $prefix;
+
+    public function __construct(string $prefix = 'config')
+    {
+        $this->prefix = $prefix;
+    }
 
     public function boot(Application $app)
     {
@@ -24,28 +30,33 @@ class ConfigServiceProvider implements ServiceProviderInterface
     private function loadConfig(Container $app)
     {
 
-        $appConfig = $this->parseConfig('app');
-        // Some config more
+        $configLoaded = [];
 
-        $config = array_merge($appConfig);
+        $files = glob(__DIR__ . '/../../app/config/*.{yml}', GLOB_BRACE);
 
-        foreach ($config as $key => $value) {
-            $app[$key] = function(Container $app) use ($value) {
-                return $value;
-            };
+        foreach ($files as $file) {
+            $config = $this->parseConfig($file);
+
+            foreach ($config as $key => $value) {
+                $configLoaded[$key] = $value;
+            }
         }
+
+        $app[$this->prefix] = function ($app) use ($configLoaded) {
+            return $configLoaded;
+        };
     }
 
-    private function parseConfig(string $fileName)
+    private function parseConfig(string $filePath) : array
     {
         $config = [];
 
         try {
-            $config = Yaml::parse(file_get_contents(__DIR__ . '/../../app/config/' . $fileName . '.yml'));
+            $config = Yaml::parse(file_get_contents($filePath));
         } catch (ParseException $e) {
             printf("Unable to parse the YAML string: %s", $e->getMessage());
         }
 
-        return $config[$fileName];
+        return $config;
     }
 }
